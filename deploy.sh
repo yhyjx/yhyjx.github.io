38a562d4c47fcc015892b4624a7b16f9b7626d77
zkml_yuhao <iyuhaoo@126.com>
2023-04-12 10:05:14 +0800

    自动发布静态资源脚本实现

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+set -e
+
+npm run docs:build
+
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m '自动构建发布'
+git push -f git@github.com:WorkaholicYuHao/WorkaholicYuHao.github.io.git pages
+
+cd -
